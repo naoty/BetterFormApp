8beffe9ecf200215a3ac5c8d00ff0991af068793
Naoto Kaneko <naoty.k@gmail.com>
2014-09-15 00:52:31 +0900

    Initial commit

---
+++ BetterFormApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BetterFormApp
+//
+//  Created by Naoto Kaneko on 2014/09/15.
+//  Copyright (c) 2014年 Naoto Kaneko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
