bde1fbbf658b1857056db7bc21f3ff58032096fe
Naoto Kaneko <naoty.k@gmail.com>
2014-09-18 01:24:01 +0900

    Add borders at bottom of text fields

--- BetterFormApp/TextFieldContainer.swift
+++ BetterFormApp/TextFieldContainer.swift
@@ -10,14 +10,36 @@ import UIKit
 
 @IBDesignable
 class TextFieldContainer: UIView {
+    private var width: CGFloat {
+        return CGRectGetWidth(frame)
+    }
+    private var height: CGFloat {
+        return CGRectGetHeight(frame)
+    }
+    private let borderBottom: CALayer = CALayer()
+    
     @IBInspectable
-    var borderWidth: CGFloat = 0 {
+    var borderColor: UIColor = UIColor.blackColor() {
         didSet {
-            layer.borderWidth = borderWidth
+            setupBorderBottom()
+        }
+    }
+    
+    @IBInspectable
+    var borderBottomWidth: CGFloat = 0 {
+        didSet {
+            setupBorderBottom()
         }
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         subviews.first?.becomeFirstResponder()
+    }
+    
+    private func setupBorderBottom() {
+        borderBottom.removeFromSuperlayer()
+        borderBottom.frame = CGRectMake(0, height - borderBottomWidth, width, borderBottomWidth)
+        borderBottom.backgroundColor = borderColor.CGColor
+        layer.addSublayer(borderBottom)
     }
 }
